5ec925984d46c0d67f9e7f77dea0e3ee680baaa6
numbergames <bfletch@gmail.com>
2016-11-07 17:26:02 -0500

    End of pair session day 1

--- app.js
+++ app.js
@@ -17,106 +17,110 @@ $(document).ready(function () {
       question: "xxxxxxx ?",
 
       answers: [
-        "answer choice 'a'.",
-        "answer choice 'b'.",
-        "answer choice 'c'.",
+        "answer choice 'a'.",  // 0 
+        "answer choice 'b'.",  // 1
+        "answer choice 'c'.",  // 2
         "answer choice 'd'.",
         "answer choice 'e'.",
       ],
 
-      correctAnswer: 2 // hardcoded answer
+      correctAnswer: 2 // hardcoded answer (0 to 4)
     }
   ];
 
   var state = (function generateState() {
-    var status = {
+    var currentQuestion = 0;  // starts at 0, incremented as questions answered
+      // questions[currentQuestion] => points at current question details
 
-      currentQuestion: 1,  // starts at 1, incremented as questions answered
-
-      numCorrect: 0, // calculate numIncorrect by subtracting
-    };
+    var numCorrect = 0; // calculate numIncorrect by subtracting
 
     // state modifying functions
-
     function generateNextQuestion() {
-      // User Joe has clicked start or next question, this fn is called to generate next/first question
+      console.log(currentQuestion, numCorrect);
 
+      var question = questions[currentQuestion].question;
+      var answers = questions[currentQuestion].answers;
 
+      var questionHtml = 
+        '<h2>Question ' + (currentQuestion + 1) + ' of ' + questions.length + ' :</h2>' +
+        '<h3 id="#question">' + question + '</h3>';
+
+      var answersHtml = 
+        '<ol type="a">' + 
+          answers.map(function (element, index) {
+            return '<li class="answers" id="js-opt' + index + '">' + element + '</li>';
+          }).join("") +
+        '</ol>' +
+        '<h3>Please click on your choice above.</h3>';
+
+      var footerHtml = 
+        '<div>' +
+          '<p id="question-footer">So far you have gotten ' + numCorrect + ' right and ' + 
+            (currentQuestion - numCorrect) + ' wrong.</p>' +
+        '</div>';
+
+      // debugger;
+      return questionHtml + answersHtml + footerHtml;
     }
 
-    function submitAnswer() {
+    function compareAnswer(answer) {
+      var correctAnswer = questions[currentQuestion].correctAnswer;
 
+      if (answer === correctAnswer) {
+        // got it right
+
+      } else {
+        // got it wrong
+        
+      }
     }
 
     function nextQuestion() {
-
+      currentQuestion += 1;
     }
 
     function resetQuiz() {
-
+      currentQuestion = 0;
+      numCorrect = 0;
     }
 
     function generateFinalResults() {
 
     }
 
-    function generate() {
-      var itemsHTML = items.map(function(item) {
-        var checkedClass = item.checked ? ' shopping-item__checked' : '';
-
-        return (
-          '<li>' +
-            '<span class="shopping-item' + checkedClass + '">' +
-              item.name + '</span>' +
-            '<div class="shopping-item-controls">' +
-              '<button class="shopping-item-toggle">' +
-                '<span class="button-label">check</span>' +
-              '</button>\n' +
-              '<button class="shopping-item-delete">' +
-                '<span class="button-label">delete</span>' +
-                '</button>' +
-            '</div>' +
-          '</li>'
-        );
-      });
-
-      return itemsHTML.join("\n");
-    }
-
     return {
-      items,  // items: items (ES6 destructuring??)
-      addItem,
-      toggleItem,
-      deleteItem,
-      generateList
+      status,
+      generateNextQuestion,
+      compareAnswer,
+      nextQuestion,
+      resetQuiz,
+      generateFinalResults
     };
   }());
 
-    // DOM manipulation
-
-    // DOM traversal (listeners & events)
-
-    // Process user input
-
+  function renderQuestion(question) {
+    $('#js-main').html(question);
+  }
 
   // Listeners
-  $('#start').click(function(){
-var next = generateNextQuestion();
-  // Flush out 106
-  console.log('Hello I was clicked');
-  renderQuestion(next);
+  $('#start').click(function () {
+    state.resetQuiz();  // do we need?
+
+    var next = state.generateNextQuestion();
+
+    renderQuestion(next);
+  });
+
+  $('.container').on('click', '.answers', function(event) {
+    // need extract '0' out of 'js-opt0'  // 'hello, world 1'
+    var selectedAnswer = Number(event.target.id.substr(-1));
+
+    console.log(selectedAnswer);
+
+    state.compareAnswer(selectedAnswer);
+
+  })
 });
 
-  // Attached to the parent preventing overwrite on render
-  $('.shopping-list')
 
-    .on('click', '.shopping-item-toggle', function(event) {
-      state.toggleItem(getButtonItem(event));
-      $('.shopping-list').html(state.generateList());
-    })
 
-    .on('click', '.shopping-item-delete', function(event) {
-      state.deleteItem(getButtonItem(event));
-      $('.shopping-list').html(state.generateList());
-    });
-});
