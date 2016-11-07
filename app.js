1e7b40f6400d32f7994ef50f7d99134746c57247
numbergames <bfletch@gmail.com>
2016-11-07 14:24:45 -0500

    Post-lunch switch

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 $(document).ready(function () {
+  var questions = [
+    {
+      question: "xxxxxxx ?",
+
+      answers: [
+        "answer choice 'a'.",
+        "answer choice 'b'.",
+        "answer choice 'c'.",
+        "answer choice 'd'.",
+        "answer choice 'e'.",
+      ],
+
+      correctAnswer: 3
+    },
+    {
+      question: "xxxxxxx ?", 
+
+      answers: [
+        "answer choice 'a'.",
+        "answer choice 'b'.",
+        "answer choice 'c'.",
+        "answer choice 'd'.",
+        "answer choice 'e'.",
+      ],
+
+      correctAnswer: 2 // hardcoded answer
+    }
+  ];
 
   var state = (function generateState() {
-    var quiz = {
-      questions: [  // these may belong outside `state` var since they're not really state
-        {
-          question: "xxxxxxx ?",
-
-          answers: [
-            "answer choice 'a'.",
-            "answer choice 'b'.",
-            "answer choice 'c'.",
-            "answer choice 'd'.",
-            "answer choice 'e'.",
-          ],
-
-          correctAnswer: 3
-        },
-        {
-          question: "xxxxxxx ?", 
-
-          answers: [
-            "answer choice 'a'.",  // answer text, order is hardcoded
-            "answer choice 'b'.",
-            "answer choice 'c'.",
-            "answer choice 'd'.",
-            "answer choice 'e'.",
-          ],
-
-          correctAnswer: 2 // hardcoded answer
-        }
-          // remainder of questions and answers go here...
-      ],
+    var status = {
 
       currentQuestion: 1,  // starts at 1, incremented as questions answered
 
-      numCorrect: 0, // dynamically updated with each anwer submission
-
-      numIncorrect: 0, // dynamically updated with each anwer submission
+      numCorrect: 0, // calculate numIncorrect by subtracting
     }
 
     // state modifying functions
 
     function generateNextQuestion() {
-      // User Joe clicks
+      // User Joe has clicked start or next question, this fn is called to generate next/first question
+
+
     }
 
     function submitAnswer() {
@@ -61,38 +60,7 @@ $(document).ready(function () {
 
     }
 
-    // DOM manipulation
-
-    // DOM traversal (listeners & events)
-
-    // Process user input
-
-
-
-
-
-
-
-
-
-    var _itemIndex = itemName => items.findIndex(
-      item => item.name === itemName);
-
-    function addItem(itemName) { 
-      return items.push({name: itemName, checked: false});
-    }
-
-    function toggleItem (itemName) {
-      var index = _itemIndex(itemName);
-
-      items[index].checked = state.items[index].checked ? false : true;
-    }
-
-    function deleteItem(itemName) {
-      items.splice(_itemIndex(itemName), 1);
-    }
-
-    function generateList() {
+    function generate... () {
       var itemsHTML = items.map(function(item) {
         var checkedClass = item.checked ? ' shopping-item__checked' : '';
 
@@ -124,8 +92,15 @@ $(document).ready(function () {
     };
   }());
 
+    // DOM manipulation
+
+    // DOM traversal (listeners & events)
+
+    // Process user input
+
+
   // Listeners
-  $('#js-shopping-list-form').submit(function(event) {
+  $('#js-question').submit(function(event) {
     event.preventDefault();
 
     var newItem = $('#shopping-list-entry').val();
