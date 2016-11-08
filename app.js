e3772b260543fe379e48c86cbf88bf5c366706fa
numbergames <bfletch@gmail.com>
2016-11-08 00:10:33 -0500

    Move questions to separate file. Formatting changes. Pseudocode for additional functions.

--- app.js
+++ app.js
@@ -1,65 +1,37 @@
 $(document).ready(function () {
-  var questions = [
-    {
-      question: "xxxxxxx ?",
-
-      answers: [
-        "answer choice 'a'.",
-        "answer choice 'b'.",
-        "answer choice 'c'.",
-        "answer choice 'd'.",
-        "answer choice 'e'.",
-      ],
-
-      correctAnswer: 3
-    },
-    {
-      question: "xxxxxxx ?",
-
-      answers: [
-        "answer choice 'a'.",  // 0 
-        "answer choice 'b'.",  // 1
-        "answer choice 'c'.",  // 2
-        "answer choice 'd'.",
-        "answer choice 'e'.",
-      ],
-
-      correctAnswer: 2 // hardcoded answer (0 to 4)
-    }
-  ];
-
   var state = (function generateState() {
-    var currentQuestion = 0;  // starts at 0, incremented as questions answered
-      // questions[currentQuestion] => points at current question details
-
-    var numCorrect = 0; // calculate numIncorrect by subtracting
+    var currentQuestion = 0;  // 0 indexed to align wtih question array
+    var numCorrect = 0;
 
     // state modifying functions
-    function generateNextQuestion() {
-      console.log(currentQuestion, numCorrect);
+    function generateStartScreen() {
+      // called if user clicks on restart / start over
+      // or can we / should we trigger a refresh instead?
+      // this is the HTML hardcoded in index.html
+    }
 
+    function generateNextQuestion() {
       var question = questions[currentQuestion].question;
       var answers = questions[currentQuestion].answers;
 
-      var questionHtml = 
+      var questionHtml =
         '<h2>Question ' + (currentQuestion + 1) + ' of ' + questions.length + ' :</h2>' +
         '<h3 id="#question">' + question + '</h3>';
 
-      var answersHtml = 
-        '<ol type="a">' + 
+      var answersHtml =
+        '<ol type="a">' +
           answers.map(function (element, index) {
             return '<li class="answers" id="js-opt' + index + '">' + element + '</li>';
           }).join("") +
         '</ol>' +
         '<h3>Please click on your choice above.</h3>';
 
-      var footerHtml = 
+      var footerHtml =
         '<div>' +
-          '<p id="question-footer">So far you have gotten ' + numCorrect + ' right and ' + 
+          '<p id="question-footer">So far you have gotten ' + numCorrect + ' right and ' +
             (currentQuestion - numCorrect) + ' wrong.</p>' +
         '</div>';
 
-      // debugger;
       return questionHtml + answersHtml + footerHtml;
     }
 
@@ -71,12 +43,17 @@ $(document).ready(function () {
 
       } else {
         // got it wrong
-        
+        // output correct answer text and choice number:
+        // "Sorry, the correct answer was 'c. blah blah blah'"
       }
     }
 
     function nextQuestion() {
       currentQuestion += 1;
+      if (currentQuestion >= questions.length) { // finished questions
+        return false; // ??? or call summarize?
+      }
+      return true; // indicates end not reached... better way to signal this?
     }
 
     function resetQuiz() {
@@ -85,7 +62,12 @@ $(document).ready(function () {
     }
 
     function generateFinalResults() {
-
+      // generate HTML for count of num right & wrong
+      // Button to start over
+      // include picture of something appropriate to results
+      // balloons or fireworks for high / perfect score
+      // crying face for low score
+      // ...
     }
 
     return {
@@ -112,15 +94,9 @@ $(document).ready(function () {
   });
 
   $('.container').on('click', '.answers', function(event) {
-    // need extract '0' out of 'js-opt0'  // 'hello, world 1'
     var selectedAnswer = Number(event.target.id.substr(-1));
-
-    console.log(selectedAnswer);
 
     state.compareAnswer(selectedAnswer);
 
   })
 });
-
-
-
---
+++ questions.js
@@ -0,0 +1,68 @@
+var questions = [
+  {
+    question: "Where did the name JavaScript originate?",
+    answers: [
+      "It was originally all about coffee and cursive writing.",
+      "It was meant to signal to developers that it was a watered down version of Java for web browsers.",
+      "It has almost nothing to do with Java, but they wanted to piggyback on its popularity.",
+      "All the good names were taken.",
+      "They thought a deceptive name was fitting for a deceptively complex language.",
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "Which of the following is NOT a JavaScript primitive data type?",
+    answers: [
+      "string",
+      "number",
+      "integer",
+      "object",
+      "array",
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "Where is the best place to declare the majority of your variables?",
+    answers: [
+      "while sitting on the toilet or taking a shower",
+      "in global so their accessible everywhere",
+      "in function scope",
+      "good programmers don't need no stinkin' variables",
+      "always, always, always ... in the comments",
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "How much should one function accomplish?",
+    answers: [
+      "as much as you can fit in 10 lines",
+      "everything",
+      "a single concern",
+      "three things, always 3",
+      "no limit as long as the code rhymes",
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "Which of the following could be valid JavaScript?",
+    answers: [
+      "var foo <= 42;",
+      "function {banana} (return banana + 'split'; )",
+      "foo = bar = baz = 42;",
+      "Math.pi('apple') = 'yummy';",
+      "do strictly;",
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: "Which of the following is an accurate statement?",
+    answers: [
+      "Web apps are easy and quick to write even for beginners.",
+      "Sooo, is this all the app can do?",
+      "This websites developers are god-like.",
+      "Did you know that browsers can do, like, colors?",
+      "I've seen three-year-olds code better sites.",
+    ],
+    correctAnswer: 3
+  }
+];
